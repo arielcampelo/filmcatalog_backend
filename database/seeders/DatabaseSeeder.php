6b31e7da9bec43d07f4b39d9a403f85b4bfa3699
jlopes <jlopes@alumni.usp.br>
2021-10-27 17:09:25 -0300

    Added a seeder for films

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $titles = array('Home alone', 'Ghost','Avengers','Jaws');
+        $authors = array('John Hughes','Bruce Rubin','Joss Whedon','Steven Spilberg');
+        $years = array(1990, 1990, 2012, 1975);
+        for ($i=0;$i <= count($titles);$i++){
+            DB::table('films')->insert([
+                'title' => $titles[$i],
+                'author' => $authors[$i],
+                'year' => $years[$i],
+            ]);
+        }
+
+
     }
 }
